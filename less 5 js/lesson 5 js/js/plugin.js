008bca4a54343a460076580fa615d3e9f8a54c97
tem44ik <tem44ik@i.ua>
2017-01-04 21:48:50 +0200

    добавлено дз по массивам

---
+++ less 5 js/lesson 5 js/js/plugin.js
@@ -0,0 +1,73 @@
+// Массивы задачи
+
+//1  (доделать!!!!!)-----------
+// var str = 'bcdaeflmjhi';
+// var newStr = str.split('')
+// var arr = newStr;
+// 	newArr = arr.sort();			
+// 	newArr = arr.reverse();
+// console.log(newArr);
+
+//2
+// var arr = [2, 4, 7, 1, -2, 10, -9];
+// var newArr = arr.sort(arrSort);
+// function arrSort(prev, next) {
+// 	if (prev < next) return -1;
+// 	if (prev > next) return 1;
+// }
+// newArr = arr.reverse();
+// console.log(newArr);
+
+//3
+// function foo(arr,) {
+// 	// body...
+// }
+
+//4
+// var arr = ['one', 2, 'three', 4];
+// var conc = arr.concat(arr);
+// console.log(conc);
+
+//5
+// var arr = [1, 2, 3, 4, 5];
+// arr.splice(2,2);
+// console.log(arr);
+
+//6
+// var arr = [1, 2, 3, 4, 5];
+// arr.splice(2,2,'three', 'four');
+// console.log(arr);
+
+//7
+// var arr = [1, 2, 3, 4, 5];
+// arr.splice(3,0,'awesome');
+// console.log(arr);
+
+//8
+// var arr = [[14, 45], [1], ['a', 'c', 'd']];
+// function arrSort(prev, next) {
+// }
+// var newArr = arr.sort(arrSort);
+// console.log(newArr);
+
+
+
+//9
+// var arr = [1, 2, 3, 'four'];
+// var newArr = arr.slice();
+// console.log(newArr);
+
+//10
+// function arrSort(prev, next) {
+// 	if (prev.info.cores < next.info.cores) return -1;
+// 	if (prev.info.cores > next.info.cores) return 1;
+// }
+// var arr = [{cpu: 'intel', info: {cores: 2, cache: 3}},
+// 			{cpu: 'intel', info: {cores: 4, cache: 4}},
+// 			{cpu: 'amd', info: {cores: 1, cache: 1}},
+// 			{cpu: 'intel', info: {cores: 3, cache: 2}},
+// 			{cpu: 'amd', info: {cores: 4, cache: 2}}];
+
+
+// var newArr = arr.sort(arrSort);
+// console.log(newArr);
